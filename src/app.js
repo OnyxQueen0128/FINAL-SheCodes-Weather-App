ea2bd23b00557043d4b2a65e1e47ddca7e9442aa
OnyxQueen0128 <107898155+OnyxQueen0128@users.noreply.github.com>
2022-08-27 20:17:48 +0200

    formatted date function

--- src/app.js
+++ src/app.js
@@ -1,14 +1,38 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return `${days[day]} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   let temperatureIndicator = document.querySelector("#maintemperature");
   let cityName = document.querySelector("#city");
   let weatherDescription = document.querySelector("#skydescription");
   let humidityIndicator = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind");
+  let dateIndicator = document.querySelector("#date");
   temperatureIndicator.innerHTML = Math.round(response.data.main.temp);
   cityName.innerHTML = response.data.name;
   weatherDescription.innerHTML = response.data.weather[0].description;
   humidityIndicator.innerHTML = response.data.main.humidity;
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  dateIndicator.innerHTML = formatDate(response.data.dt * 1000);
   console.log(response.data);
 }
 
