d49cdca4ced503ae05235436340c2c53bde2e169
OnyxQueen0128 <107898155+OnyxQueen0128@users.noreply.github.com>
2022-08-27 19:09:45 +0200

    began inserting API configs

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
- 
+function displayTemperature(response) {}
+
+let apiKey = "213b9f4d2bf744ddfe96906f0ba3da1f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Zurich&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(url).then(displayTemperature);
